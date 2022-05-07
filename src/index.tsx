cf121b8d55e33f011ad5a9e4f30839c213f2aeb1
GraceYu <librayuyueran@gmail.com>
2022-05-07 11:29:24 -0500

    install the jira-dev-tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
+import { loadDevTools } from "jira-dev-tool";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+loadDevTools(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
