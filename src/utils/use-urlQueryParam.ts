275d1c57d1f20efe012b41b702d87c2d1482cd56
GraceYu <librayuyueran@gmail.com>
2022-05-18 22:07:45 -0500

    built the useURLQueryParam  hook to extract the params from url

---
+++ src/utils/use-urlQueryParam.ts
@@ -0,0 +1,17 @@
+import { useSearchParams } from "react-router-dom";
+
+/**
+ * 返回页面url中，指定键的参数值
+ * */
+
+export const useURLQueryParam = (keys: string[]) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  return [
+    keys.reduce((prev, key) => {
+      return { ...prev, [key]: searchParams.get(key) || "" };
+    }, {} as { [key in string]: string }),
+    setSearchParams,
+  ] as const;
+};
+
+// A 'const' assertion tells the compiler to infer the narrowest or most specific type it can for an expression
